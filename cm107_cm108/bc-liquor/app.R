ca5e4217472c4e7a40886e3e61644c177f82b4e5
Stefanie Lumnitz <stefanie.lumnitz@gmail.com>
2018-11-13 09:58:59 -0800

    finish example how format ui in shiny

--- cm107_cm108/bc-liquor/app.R
+++ cm107_cm108/bc-liquor/app.R
@@ -2,7 +2,12 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    "This is text.",
+    "This is more text.",
+    "test str(tags) in console.",
+    tags$h1("Level 1 header"),
+    h1("Level 1 header part II"),
+    HTML("<h1>Level 1 header part 3</h1>")
 )
 
 # Define server logic required to draw a histogram
