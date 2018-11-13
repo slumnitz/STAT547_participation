c0e962a1ee267f8d242274e4811e8100326dd4bb
Stefanie Lumnitz <stefanie.lumnitz@gmail.com>
2018-11-13 10:22:03 -0800

    finish ui layout shiny

--- cm107_cm108/bc-liquor/app.R
+++ cm107_cm108/bc-liquor/app.R
@@ -1,13 +1,24 @@
 library(shiny)
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+a <- 5 
+print(a^2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     "This is text.",
+    br(),
     "This is more text.",
+    br(),
+    br(),
     "test str(tags) in console.",
     tags$h1("Level 1 header"),
+    tags$blockquote("Tidy data sets are all the same. Each messy data set is messy in its own way.", cite = "Hadley Wickham"),
     h1("Level 1 header part II"),
-    HTML("<h1>Level 1 header part 3</h1>")
+    tags$code("This text will be displayed as computer code."),
+    HTML("<h1>Level 1 header part 3</h1>"),
+    tags$a(href="https://shiny.rstudio.com/articles/tag-glossary.html", "link to tags document"),
+    print(a)
 )
 
 # Define server logic required to draw a histogram
