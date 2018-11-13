f50d7e7c88ae87fac9c1bc2a326c35fcf782fe24
Stefanie Lumnitz <stefanie.lumnitz@gmail.com>
2018-11-13 10:47:38 -0800

    finish building my first shiny app example

--- cm107_cm108/bc-liquor/app.R
+++ cm107_cm108/bc-liquor/app.R
@@ -6,24 +6,23 @@ print(a^2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    "This is text.",
-    br(),
-    "This is more text.",
-    br(),
-    br(),
-    "test str(tags) in console.",
-    tags$h1("Level 1 header"),
-    tags$blockquote("Tidy data sets are all the same. Each messy data set is messy in its own way.", cite = "Hadley Wickham"),
-    h1("Level 1 header part II"),
-    tags$code("This text will be displayed as computer code."),
-    HTML("<h1>Level 1 header part 3</h1>"),
-    tags$a(href="https://shiny.rstudio.com/articles/tag-glossary.html", "link to tags document"),
-    print(a)
+    titlePanel("BC Liquor price app", 
+               windowTitle = "BCL app"),
+    sidebarLayout(
+        sidebarPanel("This text is in the sidebar."),
+        mainPanel(
+            "We cannot put R code here!",
+            plotOutput("price_hist"),
+            tableOutput("bcl_data")
+            )
+    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
+    output$bcl_data <- renderTable({summary(bcl)})
+    
 }
 
 # Run the application 
